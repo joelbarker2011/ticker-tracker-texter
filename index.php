6108ec20113dcd35220f93c695530706eb665966
Joel Barker <joel@mojamail.com>
2018-01-04 20:38:11 -0800

    Initial commit of stock tracker

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+$stocks = ['VTI', 'VXUS', 'BND'];
+
+$alerts = [];
+foreach ($stocks as $stock) {
+    list($low, $high) = getBBands($stock);
+    $current = getCurrent($stock);
+
+    if ($current > $high) {
+        $alerts[] = "Sell $stock"; // ($current > $high)";
+    } elseif ($current < $low) {
+        $alerts[] = "Buy $stock"; // ($current < $low)";
+    }
+
+    sleep(rand(1, 10));
+}
+
+$response = sendAlerts($alerts);
+header('Content-Type: text/plain');
+echo $response;
+
+// helper functions
+
+function getBBands($stock) {
+    $query = http_build_query([
+        'function'      => 'BBANDS',
+        'symbol'        => $stock,
+        'interval'      => 'daily',
+        'time_period'   => 21,
+        'series_type'   => 'close',
+        'nbdevup'       => 1,
+        'nbdevdn'       => 1,
+        'matype'        => 1,   // EMA
+        'apikey'        => $_ENV['ALPHA_VANTAGE_API_KEY'],
+    ]);
+
+    $ch = curl_init('https://www.alphavantage.co/query?' . $query);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $json = json_decode($response, true);
+    $latest = reset($json['Technical Analysis: BBANDS']);   // get first value, efficiently
+
+    return [$latest['Real Lower Band'], $latest['Real Upper Band']];
+}
+
+function getCurrent($stock) {
+    $query = http_build_query([
+        'function'      => 'TIME_SERIES_INTRADAY',
+        'symbol'        => $stock,
+        'interval'      => '15min',
+        'apikey'        => $_ENV['ALPHA_VANTAGE_API_KEY'],
+    ]);
+
+    $ch = curl_init('https://www.alphavantage.co/query?' . $query);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $json = json_decode($response, true);
+    $latest = reset($json['Time Series (15min)']);  // get first value, efficiently
+
+    return $latest['4. close'];
+}
+
+function sendAlerts($alerts) {
+    $message = array_join("\n", $alerts);
+
+    $ch = curl_init($_ENV['TILL_URL']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+        'phone' => [$_ENV['RECIPIENT_PHONE']],
+        'text'  => $message,
+    ]));
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
+
